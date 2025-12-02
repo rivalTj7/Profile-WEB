bf906def5eb95bc7a9f15d81ef2c760a2dd4f8df
rivalTj7 <rival.alex7@gmail.com>
2025-12-02 00:18:36 -0600

    profile full deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
