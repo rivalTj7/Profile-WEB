96776738a5965f0961e07068295f6adfff99259f
rivalTj7 <rival.alex7@gmail.com>
2025-12-29 01:03:15 -0600

    fix: Corregida configuración para deployment nativo en Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
 }
 
 module.exports = nextConfig
